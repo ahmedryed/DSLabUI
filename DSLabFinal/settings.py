88fa047d29a318013837faea3b1df86cf15a14ae
Ryed Ahmed <ryed.ahmed@utexas.edu>
2021-05-05 18:50:08 -0500

    Working templates

--- DSLabFinal/settings.py
+++ DSLabFinal/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DSLabFinal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['DSLabFinal/templates/', '/model/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

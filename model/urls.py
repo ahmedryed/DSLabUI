c51eacf1cef2c491ba818051ecb7f38a6674695a
Matt MacDonald <mattkmacd@gmail.com>
2021-05-06 18:27:14 -0500

    idk

--- model/urls.py
+++ model/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from model import views
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.model),
-    path('about', views.about),
+    path('', views.model, name='model'),
+    path('about', views.about, name='about'),
 ]
 
 if settings.DEBUG:

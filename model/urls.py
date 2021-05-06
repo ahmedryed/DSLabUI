1292c24b7b005ff75598eba36b099074362ccb28
Matt MacDonald <mattkmacd@gmail.com>
2021-05-06 18:16:13 -0500

    idk
    still doesnt work

--- model/urls.py
+++ model/urls.py
@@ -4,9 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.model, name='views'),
-    path('about', views.about, name='about'),
+    path('', views.model),
+    path('about', views.about),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
